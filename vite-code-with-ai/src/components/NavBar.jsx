569572b228dc62eae31a6725aba647b4d49dbf51
olivvein <olivier.veinand@gmail.com>
2024-04-05 01:28:21 +0200

    Mobile NavBar

--- vite-code-with-ai/src/components/NavBar.jsx
+++ vite-code-with-ai/src/components/NavBar.jsx
@@ -12,6 +12,7 @@ const NavBar = ({
   visiblesIds,
 }) => {
   const [openMenu, setOpenMenu] = useState("");
+  const MobileView = window.innerWidth < 800 ? true : false;
 
   const toggleMenu = (menu) => {
     setOpenMenu(openMenu === menu ? "" : menu);
@@ -123,47 +124,56 @@ const NavBar = ({
     },
   ];
 
+  const [showMenu, setShowMenu] = useState(false);
+
   return (
-    <nav id="theNavbar" className="absolute w-full p-0 m-0 top-0 left-0 dark:bg-dark bg-light dark:text-light text-dark px-0 h-5 flex z-40 text-sm ">
-      <div className=" w-full top-0 dark:bg-dark bg-light dark:text-light text-dark  px-0 flex justify-between">
-        <div className="w-5/6 top-0 dark:bg-dark bg-light dark:text-light text-dark pt-1 px-0 flex">
-          <span className="left-0 top-0  px-2 flex flex-col">
-            <span className=" left-0 top-0 text-sm px-2">
-              <b>Code With Ai</b>
-            </span>
-          </span>
-          {menuConfig.map((menu, index) => (
-            <div key={index} className="relative inline-block">
-              <span
-                tabIndex={index} // Makes the div focusable
-                // Handles focus event
-                onBlur={() => {
-                  setTimeout(() => setOpenMenu(""), 100);
-                }}
-                onMouseOver={() => {
-                  handleHover(menu.name);
-                }}
-                className={`${
-                  openMenu == menu.name ? "bg-gray-700" : ""
-                } cursor-pointer pt-1 pb-1 px-2 active:bg-gray-700  hover:bg-blue-800 hover:text-light hover:shadow-lg rounded-lg mt-2`}
-                onClick={() => toggleMenu(menu.name)}
-              >
-                {menu.name}
+    <>
+      {MobileView ==false ? (
+        <nav
+          id="theNavbar"
+          className="absolute w-full p-0 m-0 top-0 left-0 dark:bg-dark bg-light dark:text-light text-dark px-0 h-5 flex z-40 text-sm "
+        >
+          <div className=" w-full top-0 dark:bg-dark bg-light dark:text-light text-dark  px-0 flex justify-between">
+            <div className="w-5/6 top-0 dark:bg-dark bg-light dark:text-light text-dark pt-1 px-0 flex">
+              <span className="left-0 top-0  px-2 flex flex-col">
+                <span className=" left-0 top-0 text-sm px-2">
+                  <b>Code With Ai</b>
+                </span>
               </span>
-              {openMenu === menu.name && (
-                <div className="absolute backdrop-blur-xl  dark:bg-dark/60 bg-light/60 dark:text-light text-dark  left-0 mt-2 w-48  rounded-md  border border-gray-700 transition-appear opacity-100 shadow-lg z-50 flex flex-col justify-around">
-                  {menu.subMenu.map((subMenu, index) => (
-                    <>
-                      {subMenu.name == "separator" ? (
-                        <div className="border-b border-gray-500 mx-2 my-1"></div>
-                      ) : (
-                        <button
-                          key={index}
-                          className={`rounded-xl  mx-1 text-left px-4 py-2 active:border text-sm  active:bg-gray-400 active:border-gray-700 active:text-dark transition-colors duration-100 ease-in-out hover:bg-blue-800/100 hover:text-light ${
-                            subMenu.status == "inactive"
-                              ? "dark:text-gray-500 text-gray-400 "
-                              : "dark:text-light text-dark "
-                          } ${index == menu.subMenu.length - 1 ? "mb-1" : ""}
+              {menuConfig.map((menu, index) => (
+                <div key={index} className="relative inline-block">
+                  <span
+                    tabIndex={index} // Makes the div focusable
+                    // Handles focus event
+                    onBlur={() => {
+                      setTimeout(() => setOpenMenu(""), 100);
+                    }}
+                    onMouseOver={() => {
+                      handleHover(menu.name);
+                    }}
+                    className={`${
+                      openMenu == menu.name ? "bg-gray-700" : ""
+                    } cursor-pointer pt-1 pb-1 px-2 active:bg-gray-700  hover:bg-blue-800 hover:text-light hover:shadow-lg rounded-lg mt-2`}
+                    onClick={() => toggleMenu(menu.name)}
+                  >
+                    {menu.name}
+                  </span>
+                  {openMenu === menu.name && (
+                    <div className="absolute backdrop-blur-xl  dark:bg-dark/60 bg-light/60 dark:text-light text-dark  left-0 mt-2 w-48  rounded-md  border border-gray-700 transition-appear opacity-100 shadow-lg z-50 flex flex-col justify-around">
+                      {menu.subMenu.map((subMenu, index) => (
+                        <>
+                          {subMenu.name == "separator" ? (
+                            <div className="border-b border-gray-500 mx-2 my-1"></div>
+                          ) : (
+                            <button
+                              key={index}
+                              className={`rounded-xl  mx-1 text-left px-4 py-2 active:border text-sm  active:bg-gray-400 active:border-gray-700 active:text-dark transition-colors duration-100 ease-in-out hover:bg-blue-800/100 hover:text-light ${
+                                subMenu.status == "inactive"
+                                  ? "dark:text-gray-500 text-gray-400 "
+                                  : "dark:text-light text-dark "
+                              } ${
+                                index == menu.subMenu.length - 1 ? "mb-1" : ""
+                              }
                       ${index == 0 ? "mt-1" : ""}
                       ${
                         subMenu.status == "inactive"
@@ -171,38 +181,120 @@ const NavBar = ({
                           : ""
                       }
                       `}
-                          // onClick={() => {
-
-                          //   sendMenuAction(subMenu.action);
-                          //   setTimeout(() => setOpenMenu(""), 300);
-
-                          // }}
-                          onMouseDown={(e) => {
-                            if (subMenu.status == "inactive") {
-                              e.preventDefault();
-                              return;
-                            }
-                            sendMenuAction(subMenu.action);
-                            setTimeout(() => setOpenMenu(""), 300);
-                          }}
-                        >
-                          {subMenu.name}
-                        </button>
-                      )}
-                    </>
-                  ))}
+                              onMouseDown={(e) => {
+                                if (subMenu.status == "inactive") {
+                                  e.preventDefault();
+                                  return;
+                                }
+                                sendMenuAction(subMenu.action);
+                                setTimeout(() => setOpenMenu(""), 300);
+                              }}
+                            >
+                              {subMenu.name}
+                            </button>
+                          )}
+                        </>
+                      ))}
+                    </div>
+                  )}
                 </div>
-              )}
+              ))}
             </div>
-          ))}
-        </div>
-        <div className="justify-end pt-1 px-0 ">
-          <span className="cursor-pointer  pt-1 pb-1 px-2  hover:bg-gray-700 hover:shadow-lg rounded-lg">
-            {visibleApiKey + username}
-          </span>
-        </div>
-      </div>
-    </nav>
+            <div className="justify-end pt-1 px-0 ">
+              <span className="cursor-pointer  pt-1 pb-1 px-2  hover:bg-gray-700 hover:shadow-lg rounded-lg">
+                {visibleApiKey + username}
+              </span>
+            </div>
+          </div>
+        </nav>
+      ) : (
+        <nav
+          id="theNavbar"
+          className="fixed w-full p-0 m-0 top-0 left-0 dark:bg-dark bg-light dark:text-light text-dark px-0 h-5 flex z-40 text-sm"
+        >
+          <div  tabIndex="9999942"  onBlur={() => {
+                      setTimeout(() => {if(openMenu==="okokokok"){setShowMenu(false)}}, 300);
+                    }} className="w-full top-0 dark:bg-dark bg-light dark:text-light text-dark px-0 flex flex-col">
+            <div className="w-full top-0 dark:bg-dark bg-light dark:text-light text-dark pt-1 px-0 flex justify-between">
+              <span className="left-0 top-0 px-2 flex flex-col">
+                <span className="left-0 top-0 text-sm px-2 cursor-pointer"   onClick={(e)=>{e.preventDefault();setShowMenu(!showMenu)}}>
+                  <b>Code With Ai</b>
+                </span>
+              </span>
+              <div className="justify-end pt-1 px-0">
+                <span className="cursor-pointer pt-1 pb-1 px-2 hover:bg-gray-700 hover:shadow-lg rounded-lg">
+                  {visibleApiKey + username}
+                </span>
+              </div>
+            </div>
+            {showMenu && (
+            <div className="w-full top-0 dark:bg-dark bg-light dark:text-light text-dark px-0 flex flex-col">
+              {menuConfig.map((menu, index) => (
+                <div key={index} className="relative">
+                  <span
+                    tabIndex={index}
+                    onBlur={() => {
+                      setTimeout(() => {setOpenMenu("");setShowMenu(false)}, 100);
+                    }}
+                    onMouseOver={() => {
+                      handleHover(menu.name);
+                    }}
+                    className={`${
+                      openMenu == menu.name ? "bg-gray-700" : ""
+                    } cursor-pointer pt-1 pb-1 px-2 active:bg-gray-700 hover:bg-blue-800 hover:text-light hover:shadow-lg rounded-lg mt-2`}
+                    onClick={() => toggleMenu(menu.name)}
+                  >
+                    {menu.name}
+                  </span>
+                  {openMenu === menu.name && (
+                    <div className="absolute ml-20 w-2/3 top-0 right-0 backdrop-blur-xl dark:bg-dark/60 bg-light/60 dark:text-light text-dark left-0 mt-2 w-full rounded-md border border-gray-700 transition-appear opacity-100 shadow-lg z-50 flex flex-col justify-around">
+                      {menu.subMenu.map((subMenu, index) => (
+                        <>
+                          {subMenu.name == "separator" ? (
+                            <div className="border-b border-gray-500 mx-2 my-1"></div>
+                          ) : (
+                            <button
+                              key={index}
+                              className={`rounded-xl mx-1 text-left px-4 py-2 active:border text-sm active:bg-gray-400 active:border-gray-700 active:text-dark transition-colors duration-100 ease-in-out hover:bg-blue-800/100 hover:text-light ${
+                                subMenu.status == "inactive"
+                                  ? "dark:text-gray-500 text-gray-400 "
+                                  : "dark:text-light text-dark "
+                              } ${
+                                index == menu.subMenu.length - 1 ? "mb-1" : ""
+                              }
+${index == 0 ? "mt-1" : ""}
+${
+  subMenu.status == "inactive"
+    ? " active:bg-none active:border-dark/0 active:bg-dark/0 active:dark:text-gray-600 active:text-gray-400 hover:bg-dark/0 dark:text-gray-600 text-gray-400 hover:dark:text-gray-600 hover:text-gray-500 cursor-auto"
+    : ""
+}
+ `}
+                              onMouseDown={(e) => {
+                                if (subMenu.status == "inactive") {
+                                  e.preventDefault();
+                                  return;
+                                }
+                                //setShowMenu(false);
+                                sendMenuAction(subMenu.action);
+                                
+                                setTimeout(() => {setOpenMenu(""),setShowMenu(false);}, 300);
+                              }}
+                            >
+                              {subMenu.name}
+                            </button>
+                          )}
+                        </>
+                      ))}
+                    </div>
+                  )}
+                </div>
+              ))}
+            </div>
+            )}
+          </div>
+        </nav>
+      )}
+    </>
   );
 };
 
